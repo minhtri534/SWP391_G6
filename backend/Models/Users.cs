232dbbb9990217615b06b8b2c759eecdf7bb9270
NgHogHuy <nhoanghuy2k5@gmail.com>
2025-06-20 14:02:12 +0700

    Add backend folder

---
+++ backend/Models/Users.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Models
+{
+    public class Users
+    {
+        public int userId { get; set; }
+        public string userName { get; set; } = string.Empty;
+        public int age { get; set; } = string.Empty;
+        public String gender { get; set; } = string.Empty;
+        public String password{ get; set; } 
+        public  int roleId { get; set; } =
+    }
+}
