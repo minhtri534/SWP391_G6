2e2712d28910d4f84123e52cae06da19397564a2
NgHogHuy <nhoanghuy2k5@gmail.com>
2025-06-20 14:02:12 +0700

    Add backend folder

---
+++ backend/Data/AppDBContext.cs
@@ -0,0 +1,20 @@
+﻿using backend.Models; // namespace chứa các model C#
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+          : base(options)
+        {
+        }
+
+        
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<CoachInfo> CoachInfos { get; set; }
+        public DbSet<Post> Posts { get; set; }
+  
+    }
+}
