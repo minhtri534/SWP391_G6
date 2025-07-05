2e8b82944bdc76e7f055982fd28abfc5d14951af
NguyenVoManhHung <usernameabc124@gmail.com>
2025-07-05 14:42:09 +0700

    Added daily progress tracking for coach

---
+++ backend/Controllers/TrackProgressController.cs
@@ -0,0 +1,33 @@
+using backend.Data;
+using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrackProgressController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TrackProgressController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetMemberDailyProgress(int userId)
+        {
+            var result = _context.DailyProgresses
+                .Where(a => a.UserId == userId)
+                .OrderByDescending(a => a.Date);
+            if (result.IsNullOrEmpty())
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+    }
+}
