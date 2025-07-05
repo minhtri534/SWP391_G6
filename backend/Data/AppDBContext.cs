810aac2a51dee0ca741110f28315e67dfb043cdd
NgHogHuy <nhoanghuy2k5@gmail.com>
2025-07-05 15:42:39 +0700

    fix

--- backend/Data/AppDBContext.cs
+++ backend/Data/AppDBContext.cs
@@ -1,28 +1,28 @@
 ﻿using backend.Data;
 using Microsoft.EntityFrameworkCore;
 using backend.Entities;
+using backend.Models;
 
 namespace backend.Data
 {
-  public class AppDbContext : DbContext
-  {
-    public AppDbContext(DbContextOptions<AppDbContext> options)
-      : base(options)
+    public class AppDbContext : DbContext
     {
-    }
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+          : base(options)
+        {
+        }
 
 
-    public DbSet<Registration> Users { get; set; }
-    public DbSet<SmokingStatus> SmokingStatuses { get; set; }
-    public DbSet<QuitPlan> QuitPlans { get; set; }
-    public DbSet<PlanMilestone> PlanMilestones { get; set; }
-    public DbSet<DailyProgress> DailyProgresses { get; set; }
-    public DbSet<Chat> ChatLog { get; set; }
-    public DbSet<Badge> Badges { get; set; }
-    public DbSet<CoachPlanBadge> CoachPlanBadges { get; set; }
-    public DbSet<Notification> Notifications { get; set; }
-    public DbSet<User> Users { get; set; }
-    }
-        
+        public DbSet<SmokingStatus> SmokingStatuses { get; set; }
+        public DbSet<QuitPlan> QuitPlans { get; set; }
+        public DbSet<PlanMilestone> PlanMilestones { get; set; }
+        public DbSet<DailyProgress> DailyProgresses { get; set; }
+        public DbSet<Chat> ChatLog { get; set; }
+        public DbSet<Badge> Badges { get; set; }
+        public DbSet<CoachPlanBadge> CoachPlanBadges { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<User> Users { get; set; }
     }
+ 
+    
 }
