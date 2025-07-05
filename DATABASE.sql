923f6bba49a51a890202b01062fecb04e09de3d3
LeQuoc <quocthancongdai119@gmail.com>
2025-07-05 09:16:41 +0700

    Add files via upload

--- DATABASE.sql
+++ DATABASE.sql
@@ -1,3 +1,6 @@
+DROP DATABASE IF EXISTS SWP391;
+GO
+
 CREATE DATABASE SWP391;
 GO
 
@@ -5,41 +8,105 @@ USE SWP391;
 GO
 
 CREATE TABLE role (
-  roleId INT IDENTITY(1,1) PRIMARY KEY,
-  roleName NVARCHAR(100) NOT NULL
+  roleId INT PRIMARY KEY,
+  roleName VARCHAR(100)
 );
 GO
 
 CREATE TABLE users (
   userId INT IDENTITY(1,1) PRIMARY KEY,
-  userName NVARCHAR(100) NOT NULL,
-  age INT NOT NULL,
-  gender NVARCHAR(10) NOT NULL,
-  phoneNum NVARCHAR(20) NOT NULL,
-  password NVARCHAR(100) NOT NULL,
-  roleId INT NOT NULL,
-  status NVARCHAR(50) DEFAULT 'Active',
-  joinDate DATE DEFAULT GETDATE(),
+  userName VARCHAR(100),
+  age INT,
+  gender VARCHAR(10),
+  phoneNum VARCHAR(20),
+  password VARCHAR(100),
+  roleId INT,
+  status VARCHAR(50),
+  joinDate DATE,
   FOREIGN KEY (roleId) REFERENCES role(roleId)
 );
 GO
 
 CREATE TABLE coach_info (
   coachId INT IDENTITY(1,1) PRIMARY KEY,
-  userId INT NOT NULL,
-  phoneNum NVARCHAR(20),
+  userId INT,
+  phoneNum VARCHAR(20),
   experience INT,
-  available_time NVARCHAR(100),
-  specialty NVARCHAR(100),
+  available_time VARCHAR(100),
+  specialty VARCHAR(100),
+  FOREIGN KEY (userId) REFERENCES users(userId)
+);
+GO
+
+CREATE TABLE badge (
+  badgeId INT IDENTITY(1,1) PRIMARY KEY,
+  badgeName VARCHAR(100),
+  description TEXT,
+  condition_type VARCHAR(50),
+  value INT
+);
+GO
+
+CREATE TABLE plan_milestone (
+  milestoneId INT IDENTITY(1,1) PRIMARY KEY,
+  planId INT NULL,
+  badgeId INT,
+  title VARCHAR(100),
+  description TEXT,
+  target_date DATE,
+  FOREIGN KEY (badgeId) REFERENCES badge(badgeId)
+);
+GO
+
+CREATE TABLE smoking_status (
+  statusId INT IDENTITY(1,1) PRIMARY KEY,
+  userId INT,
+  milestoneId INT,
+  time_period VARCHAR(50),
+  amount_per_day INT,
+  frequency VARCHAR(50),
+  price_per_pack DECIMAL(10, 2),
+  description TEXT,
+  FOREIGN KEY (userId) REFERENCES users(userId),
+  FOREIGN KEY (milestoneId) REFERENCES plan_milestone(milestoneId)
+);
+GO
+
+CREATE TABLE quit_plan (
+  planId INT IDENTITY(1,1) PRIMARY KEY,
+  userId INT,
+  coachId INT,
+  statusId INT,
+  reason TEXT,
+  start_date DATE,
+  goal_date DATE,
+  FOREIGN KEY (userId) REFERENCES users(userId),
+  FOREIGN KEY (coachId) REFERENCES coach_info(coachId),
+  FOREIGN KEY (statusId) REFERENCES smoking_status(statusId)
+);
+GO
+
+ALTER TABLE plan_milestone
+ADD FOREIGN KEY (planId) REFERENCES quit_plan(planId);
+GO
+
+CREATE TABLE daily_progress (
+  progressId INT IDENTITY(1,1) PRIMARY KEY,
+  userId INT,
+  note TEXT,
+  no_smoking BIT,
+  symptoms TEXT,
+  date DATE,
   FOREIGN KEY (userId) REFERENCES users(userId)
 );
 GO
 
 CREATE TABLE post (
-  postId INT IDENTITY(1,1) PRIMARY KEY,
+  postId INT IDENTITY(1,1) PRIMARY KEY,  
   userId INT NOT NULL,
+  title TEXT NOT NULL,
   content TEXT NOT NULL,
-  create_date DATETIME DEFAULT GETDATE(),
+  create_date DATETIME NOT NULL,         
   FOREIGN KEY (userId) REFERENCES users(userId)
 );
 GO
@@ -49,7 +116,7 @@ CREATE TABLE comment (
   postId INT,
   userId INT,
   content TEXT,
-  created_date DATE DEFAULT GETDATE(),
+  created_date DATE,
   FOREIGN KEY (postId) REFERENCES post(postId),
   FOREIGN KEY (userId) REFERENCES users(userId)
 );
@@ -61,69 +128,10 @@ CREATE TABLE report (
   postId INT,
   userId INT,
   commentId INT,
-  create_day DATE DEFAULT GETDATE(),
+  create_day DATE,
   FOREIGN KEY (postId) REFERENCES post(postId),
   FOREIGN KEY (userId) REFERENCES users(userId),
   FOREIGN KEY (commentId) REFERENCES comment(commentId)
-);
-GO
-
-CREATE TABLE quit_plan (
-  planId INT IDENTITY(1,1) PRIMARY KEY,
-  userId INT NOT NULL,
-  coachId INT NOT NULL,
-  statusId INT,
-  reason TEXT,
-  start_date DATE,
-  goal_date DATE,
-  FOREIGN KEY (userId) REFERENCES users(userId),
-  FOREIGN KEY (coachId) REFERENCES coach_info(coachId)
-);
-GO
-
-CREATE TABLE badge (
-  badgeId INT IDENTITY(1,1) PRIMARY KEY,
-  badgeName NVARCHAR(100),
-  description TEXT,
-  condition_type NVARCHAR(50),
-  value INT
-);
-GO
-
-CREATE TABLE plan_milestone (
-  milestoneId INT IDENTITY(1,1) PRIMARY KEY,
-  planId INT,
-  badgeId INT,
-  title NVARCHAR(100),
-  description TEXT,
-  target_date DATE,
-  FOREIGN KEY (planId) REFERENCES quit_plan(planId),
-  FOREIGN KEY (badgeId) REFERENCES badge(badgeId)
-);
-GO
-
-CREATE TABLE daily_progress (
-  progressId INT IDENTITY(1,1) PRIMARY KEY,
-  milestoneId INT,
-  note TEXT,
-  no_smoking BIT,
-  symptoms TEXT,
-  date DATE DEFAULT GETDATE(),
-  FOREIGN KEY (milestoneId) REFERENCES plan_milestone(milestoneId)
-);
-GO
-
-CREATE TABLE smoking_status (
-  statusId INT IDENTITY(1,1) PRIMARY KEY,
-  userId INT,
-  time_period NVARCHAR(50),
-  milestoneId INT,
-  amount_per_day INT,
-  frequency NVARCHAR(50),
-  price_per_pack DECIMAL(10, 2),
-  description TEXT,
-  FOREIGN KEY (userId) REFERENCES users(userId),
-  FOREIGN KEY (milestoneId) REFERENCES plan_milestone(milestoneId)
 );
 GO
 
@@ -145,8 +153,8 @@ CREATE TABLE notification (
   relatedLogId INT NULL,
   relatedMilestoneId INT NULL,
   message TEXT,
-  send_date DATE DEFAULT GETDATE(),
-  type NVARCHAR(50),
+  send_date DATE,
+  type VARCHAR(50),
   FOREIGN KEY (userId) REFERENCES users(userId),
   FOREIGN KEY (relatedLogId) REFERENCES daily_progress(progressId),
   FOREIGN KEY (relatedMilestoneId) REFERENCES plan_milestone(milestoneId)
@@ -160,7 +168,7 @@ CREATE TABLE feedback (
   planId INT,
   content TEXT,
   rating INT,
-  time_created DATE DEFAULT GETDATE(),
+  time_created DATE,
   FOREIGN KEY (userId) REFERENCES users(userId),
   FOREIGN KEY (coachId) REFERENCES coach_info(coachId),
   FOREIGN KEY (planId) REFERENCES quit_plan(planId)
@@ -169,7 +177,7 @@ GO
 
 CREATE TABLE membership (
   membershipId INT IDENTITY(1,1) PRIMARY KEY,
-  membershipName NVARCHAR(100),
+  membershipName VARCHAR(100),
   price DECIMAL(10, 2),
   duration INT
 );
@@ -191,10 +199,10 @@ CREATE TABLE payment (
   userId_fk INT,
   membershipId_fk INT,
   amount DECIMAL(10, 2),
-  pay_date DATE DEFAULT GETDATE(),
-  method NVARCHAR(50),
-  type NVARCHAR(50),
-  status NVARCHAR(50),
+  pay_date DATE,
+  method VARCHAR(50),
+  type VARCHAR(50),
+  status VARCHAR(50),
   FOREIGN KEY (userId_fk, membershipId_fk) REFERENCES user_memberships(userId, membershipId)
 );
 GO
@@ -204,10 +212,10 @@ CREATE TABLE chat_log (
   userId INT,
   coachId INT,
   content TEXT,
-  type NVARCHAR(50),
-  status NVARCHAR(50),
-  chat_date DATE DEFAULT GETDATE(),
-  sender NVARCHAR(50),
+  type VARCHAR(50),
+  status VARCHAR(50),
+  chat_date DATE,
+  sender VARCHAR(50),
   FOREIGN KEY (userId) REFERENCES users(userId),
   FOREIGN KEY (coachId) REFERENCES coach_info(coachId)
 );
@@ -218,27 +226,58 @@ CREATE TABLE booking_consultation (
   userId INT,
   coachId INT,
   date DATE,
-  type NVARCHAR(50),
-  status NVARCHAR(50),
+  type VARCHAR(50),
+  status VARCHAR(50),
   FOREIGN KEY (userId) REFERENCES users(userId),
   FOREIGN KEY (coachId) REFERENCES coach_info(coachId)
 );
 GO
 
--- Insert mặc định role
-INSERT INTO role (roleName)
-VALUES 
-('Guest'), 
-('Member'), 
-('Coach'), 
-('Admin');
+CREATE TABLE transaction_money (
+  transactionId INT IDENTITY(1,1) PRIMARY KEY,
+  memberId INT,
+  coachId INT,
+  planId INT,
+  amount DECIMAL(10, 2),
+  status VARCHAR(50),
+  method VARCHAR(50),
+  transaction_date DATE,
+  FOREIGN KEY (memberId) REFERENCES users(userId),
+  FOREIGN KEY (coachId) REFERENCES coach_info(coachId),
+  FOREIGN KEY (planId) REFERENCES quit_plan(planId)
+);
 GO
 
--- Insert user (không cần truyền userId vì đã có IDENTITY)
+CREATE TABLE user_badge (
+  userId INT,
+  badgeId INT,
+  date_awarded DATE,
+  PRIMARY KEY (userId, badgeId),
+  FOREIGN KEY (userId) REFERENCES users(userId),
+  FOREIGN KEY (badgeId) REFERENCES badge(badgeId)
+);
+GO
+
+
+
+INSERT INTO role (roleId, roleName)
+VALUES 
+(1, 'Guest'),
+(2, 'Member'),
+(3, 'Coach'),
+(4, 'Admin');
+GO
+
 INSERT INTO users (userName, age, gender, phoneNum, password, roleId, status, joinDate)
 VALUES 
 ('Nguyen Van A', 25, 'Male', '0901000001', 'guest123', 1, 'Active', '2025-06-01'),
 ('Tran Van B', 28, 'Female', '0901000002', 'member123', 2, 'Active', '2025-06-02'),
 ('Pham Thi C', 35, 'Male', '0901000003', 'coach123', 3, 'Active', '2025-06-03'),
-('Do Cao D', 30, 'Other', '0901000004', 'admin123', 4, 'Active', '2025-06-04');
+('Do cao D', 30, 'Other', '0901000004', 'admin123', 4, 'Active', '2025-06-04');
 GO
+
+INSERT INTO coach_info (userId, phoneNum, experience, available_time, specialty)
+VALUES (3, '0901000003', 5, 'Weekdays 9am-5pm', 'Smoking Cessation');
+GO
+
+
