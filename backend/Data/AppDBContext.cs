8f67c5926f00e8f66ed36721ef4f599853209ed5
Anonymus123-11 <quocthancongdai119@gmail.com>
2025-07-05 15:40:11 +0700

    Push toàn bộ source SWP391_G6-main

--- backend/Data/AppDBContext.cs
+++ backend/Data/AppDBContext.cs
@@ -1,51 +1,56 @@
 using backend.Data;
 using backend.Entities; // namespace chứa các model C#
+using backend.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace backend.Data
 {
-  public class AppDbContext : DbContext
-  {
-    public AppDbContext(DbContextOptions<AppDbContext> options)
-      : base(options)
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+          : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Membership> Memberships { get; set; }
+        public DbSet<UserMembership> UserMemberships { get; set; }
+        public DbSet<PaymentMemberShip> PaymentMemberships { get; set; }
+        public DbSet<CoachInfo> CoachInfos { get; set; }
+        public DbSet<PlanMilestone> PlanMilestones { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<SmokingStatus> SmokingStatuses { get; set; }
+        public DbSet<CoachInfo> CoacheInfos { get; set; }
+        public DbSet<BookingConsultation> BookingConsultations { get; set; }
+        public DbSet<QuitPlan> QuitPlans { get; set; }
+        public DbSet<DailyProgress> DailyProgresses { get; set; }
+        public DbSet<Chat> ChatLog { get; set; }
+        public DbSet<Badge> Badges { get; set; }
+        public DbSet<UserBadge> UserBadges { get; set; }
+        public DbSet<CoachPlanBadge> CoachPlanBadges { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Report> Reports { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().ToTable("users").HasKey(u => u.UserId);
+            modelBuilder.Entity<Post>().ToTable("post").HasKey(p => p.PostId);
+            modelBuilder.Entity<Comment>().ToTable("comment").HasKey(c => c.CommentId);
+            modelBuilder.Entity<Role>().ToTable("role").HasKey(r => r.RoleId);
+            modelBuilder.Entity<User>().HasOne(u => u.Role).WithMany(r => r.Users).HasForeignKey(u => u.RoleId);
+            modelBuilder.Entity<Report>().ToTable("report").HasKey(r => r.ReportId);
+            modelBuilder.Entity<Notification>().ToTable("notification").HasKey(n => n.NotificationId);
+            modelBuilder.Entity<DailyProgress>().ToTable("daily_progress").HasKey(dp => dp.ProgressId);
+            modelBuilder.Entity<Feedback>().ToTable("feedback").HasKey(f => f.FeedbackId);
+            modelBuilder.Entity<CoachInfo>().ToTable("coach_info").HasKey(c => c.CoachId);
+            modelBuilder.Entity<QuitPlan>().ToTable("quit_plan").HasKey(q => q.PlanId);
+        }
     }
-
-    public DbSet<User> Users { get; set; }
-    public DbSet<Membership> Memberships { get; set; }
-    public DbSet<UserMembership> UserMemberships { get; set; }
-    public DbSet<PaymentMemberShip> PaymentMemberships { get; set; }
-    public DbSet<CoachInfo> CoachInfos { get; set; }
-    public DbSet<PlanMilestone> PlanMilestones { get; set; }
-    public DbSet<Role> Roles { get; set; }
-    public DbSet<SmokingStatus> SmokingStatuses { get; set; }
-    public DbSet<CoachInfo> CoacheInfos { get; set; }
-    public DbSet<BookingConsultation> BookingConsultations { get; set; }
-    public DbSet<QuitPlan> QuitPlans { get; set; }
-    public DbSet<DailyProgress> DailyProgresses { get; set; }
-    public DbSet<Chat> ChatLog { get; set; }
-    public DbSet<Badge> Badges { get; set; }
-    public DbSet<UserBadge> UserBadges { get; set; }
-    public DbSet<CoachPlanBadge> CoachPlanBadges { get; set; }
-    public DbSet<Notification> Notifications { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-      base.OnModelCreating(modelBuilder);
-
-      modelBuilder.Entity<User>().ToTable("users").HasKey(u => u.UserId);
-      modelBuilder.Entity<Post>().ToTable("post").HasKey(p => p.PostId);
-      modelBuilder.Entity<Comment>().ToTable("comment").HasKey(c => c.CommentId);
-      modelBuilder.Entity<Role>().ToTable("role").HasKey(r => r.RoleId);
-      modelBuilder.Entity<User>().HasOne(u => u.Role).WithMany(r => r.Users).HasForeignKey(u => u.RoleId);
-      modelBuilder.Entity<Report>().ToTable("report").HasKey(r => r.ReportId);
-      modelBuilder.Entity<Notification>().ToTable("notification").HasKey(n => n.NotificationId);
-      modelBuilder.Entity<DailyProgress>().ToTable("daily_progress").HasKey(dp => dp.ProgressId);
-      modelBuilder.Entity<Feedback>().ToTable("feedback").HasKey(f => f.FeedbackId);
-      modelBuilder.Entity<CoachInfo>().ToTable("coach_info").HasKey(c => c.CoachId);
-      modelBuilder.Entity<QuitPlan>().ToTable("quit_plan").HasKey(q => q.PlanId);
-    }
-  }
 
 
 
