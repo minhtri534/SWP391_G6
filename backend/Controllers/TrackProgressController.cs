69ffb2810a3794d64b145b7486e3f61c6ed7de5f
NguyenVoManhHung <usernameabc124@gmail.com>
2025-07-14 16:21:46 +0700

    Added searching by date

--- backend/Controllers/TrackProgressController.cs
+++ backend/Controllers/TrackProgressController.cs
@@ -18,17 +18,33 @@ namespace backend.Controllers
             _context = context;
         }
 
-        [HttpGet("{userId}")]
-        public async Task<IActionResult> GetMemberDailyProgress(int userId)
+        [HttpGet]
+        public async Task<IActionResult> GetMemberDailyProgress([FromQuery] int userId, [FromQuery] DateTime? date = null)
         {
-            var result = _context.DailyProgresses
-                .Where(a => a.UserId == userId)
-                .OrderByDescending(a => a.Date);
-            if (result.IsNullOrEmpty())
+            if (date == null)
             {
-                return NotFound();
+                var result = _context.DailyProgresses
+                    .Where(a => a.UserId == userId)
+                    .Select(a => new { Note = a.Note, No_Smoking = a.No_Smoking, Symptoms = a.Symptoms, Date = a.Date })
+                    .OrderByDescending(a => a.Date);
+                if (result.IsNullOrEmpty())
+                {
+                    return NotFound();
+                }
+                return Ok(result);
             }
-            return Ok(result);
+            else
+            {
+                var result = _context.DailyProgresses
+                .Where(a => a.UserId == userId && date.Value.Date.CompareTo(a.Date.Date) == 0)
+                .Select(a => new { Note = a.Note, No_Smoking = a.No_Smoking, Symptoms = a.Symptoms, Date = a.Date });
+                if (result.IsNullOrEmpty())
+                {
+                    return NotFound();
+                }
+                return Ok(result);
+            }
+
         }
     }
 }
