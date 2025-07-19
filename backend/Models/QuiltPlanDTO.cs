7346c6c94b76302dfbcc9f0409b9d73c4b99877e
NgHogHuy <nhoanghuy2k5@gmail.com>
2025-07-19 12:41:14 +0700

    add more api for quit plan

---
+++ backend/Models/QuiltPlanDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace backend.Models;
+public class QuitPlanDto
+{
+    public int PlanId { get; set; }
+    public int? UserId { get; set; }
+    public int? CoachId { get; set; }
+    public int? StatusId { get; set; }
+    public string Reason { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? GoalDate { get; set; }
+}
