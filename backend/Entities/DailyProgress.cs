b60c3cddc1593997b553271cf5e18b4ff21f6988
NgHogHuy <nhoanghuy2k5@gmail.com>
2025-07-27 21:51:58 +0700

    Add DailyProgress controller and request model

--- backend/Entities/DailyProgress.cs
+++ backend/Entities/DailyProgress.cs
@@ -12,9 +12,13 @@ namespace backend.Entities
         [Column("progressId")]
         public int ProgressId { get; set; }
         public string? Note { get; set; }
+        [Column("no_smoking")]
         public bool No_Smoking { get; set; }
+        [Column("symptoms")]
         public string? Symptoms { get; set; }
+        [Column("date")]
         public DateTime Date { get; set; }
+        [Column("userId")]
         public int? UserId { get; set; }
         
         [ForeignKey(nameof(UserId))]
